7e69fecd630d65353793a2c6e9a4a45197288131
reveur96 <dlfrlwkd11@daum.net>
2023-10-09 17:23:34 +0900

    ADD : create new event function

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
   {
@@ -37,8 +39,13 @@ const router = createBrowserRouter([
   },
 ]);
 
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
 
 export default App;
